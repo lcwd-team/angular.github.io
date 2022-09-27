0663b77ff5a62b2cdec0c71a7897951432aca1f6
LearnCodeWithDurgesh <durgeshkumar8896@gmail.com>
2022-09-27 21:40:18 +0530

    updated base url

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,22 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  base_url='http://localhost:8081'
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   //create new user
-  createUser(user:any){
-    return this.httpClient.post(`${this.base_url}/users/`,user)
+  createUser(user: any) {
+    return this.httpClient.post(`${environment.baseUrl}/users/`, user)
   }
 
   //login request
-  generateToken(loginData:any){
-    return  this.httpClient.post(`${this.base_url}/auth/login`,loginData)
+  generateToken(loginData: any) {
+    return this.httpClient.post(`${environment.baseUrl}/auth/login`, loginData)
   }
 }
